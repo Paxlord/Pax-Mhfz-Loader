83c9ef50b2329c86a702dd666e4c684e7ac795b5
Paxlord <faresboukhezna@gmail.com>
2024-04-18 21:48:06 +0200

    basic modloader header file

---
+++ Pax-Mhfz-Loader/ModManager.cpp
@@ -0,0 +1,1 @@
+#include "ModManager.h"
--- Pax-Mhfz-Loader/ModManager.h
+++ Pax-Mhfz-Loader/ModManager.h
@@ -1,8 +1,21 @@
 #pragma once
 #include <Windows.h>
+#include <vector>
+#include <iostream>
+
+#include "mod.h"
 
 class ModManager
 {
+private:
+	std::vector<Mod*> mod_list;
+	static ModManager* instance;
+	ModManager();
+public:
+	static ModManager* get_instance();
+	void AttachAll();
+	void DetachAll();
+	void makeModList();
 
 };
 
