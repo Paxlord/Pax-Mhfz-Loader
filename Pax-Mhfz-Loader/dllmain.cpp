fd4a726e84ecb103dac9a65c99c72d0e54212682
Paxlord <faresboukhezna@gmail.com>
2024-04-18 15:16:59 +0200

    boiler plate dll attach rountine

--- Pax-Mhfz-Loader/dllmain.cpp
+++ Pax-Mhfz-Loader/dllmain.cpp
@@ -1,5 +1,44 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <Windows.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+HINSTANCE dll_handle;
+DWORD* mhfdll_addy = nullptr;
+
+DWORD __stdcall EjectThread(LPVOID lpParameter) {
+    Sleep(100);
+    FreeLibraryAndExitThread(dll_handle, 0);
+    return 0;
+}
+
+DWORD WINAPI Loader(HMODULE base) {
+
+    AllocConsole();
+    FILE* fp;
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+
+    do {
+        mhfdll_addy = (DWORD*)GetModuleHandleA("mhfo-hd.dll");
+        Sleep(50);
+    } while (!mhfdll_addy);
+
+    std::cout << "DLLMAIN : mhfo-hd.dll addy found : " << mhfdll_addy << std::endl;
+
+    while (true) {
+        Sleep(50);
+    }
+
+    Sleep(1000);
+    if (fp != nullptr) {
+        fclose(fp);
+    }
+    FreeConsole();
+    CreateThread(0, 0, EjectThread, 0, 0, 0);
+    return 0;
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -10,6 +49,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
+        dll_handle = hModule;
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Loader, NULL, 0, NULL);
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
